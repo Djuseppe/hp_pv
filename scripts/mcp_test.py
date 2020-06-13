76ad7a339584b7522bc5462368e4949c13e9135f
Eugene Kachalouski <kachalovsky@gmail.com>
2020-06-13 09:46:12 +0200

    added mcp_test from rp42n

--- scripts/mcp_test.py
+++ scripts/mcp_test.py
@@ -4,22 +4,25 @@ import busio
 import board
 import digitalio
 import sys
+import RPi.GPIO as GPIO
 
 import adafruit_mcp3xxx.mcp3008 as MCP
 from adafruit_mcp3xxx.analog_in import AnalogIn
 spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
-cs = digitalio.DigitalInOut(board.D5)
+cs = digitalio.DigitalInOut(board.D21)
 mcp = MCP.MCP3008(spi, cs)
 
-ratio = 300 / 2
+ratio = 10 / 2
 # if len(sys.argv) > 1:
 # 	ch_num = sys.argv[1]
 # else:
-# 	ch_num = 
-channel = AnalogIn(mcp, MCP.P7)
+# 	ch_num = ;;
+ch3 = AnalogIn(mcp, MCP.P3)
+ch4 = AnalogIn(mcp, MCP.P4)
+ch5 = AnalogIn(mcp, MCP.P5)
 while True:
 	# print('Raw ADC Value: ', channel.value)
-	ch_volt = channel.voltage
-	print('Channel voltage = {:.2f}'.format(ch_volt))
+
+	print('ch3 = {:.2f}, ch4 = {:.2f}, ch5 = {:.2f}'.format(ch3.voltage, ch3.voltage, ch5.voltage))
 	# print('ADC Voltage: {:.3f} V = {:.3f} V'.format(ch_volt, ch_volt * ratio))
 	time.sleep(1)
