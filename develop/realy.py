70852341d2448380494fd4f4ead253faaf22fadb
Eugene Kachalouski <kachalovsky@gmail.com>
2020-06-13 10:05:20 +0200

    mod realy from laptop

--- develop/realy.py
+++ develop/realy.py
@@ -1,33 +1,56 @@
 import RPi.GPIO as GPIO
 import time
+from functools import wraps
+import logging
+
+logger = logging.getLogger(__name__)
+logging.basicConfig()
+logger.setLevel(logging.DEBUG)
+stream_handler = logging.StreamHandler()
+
+
+class Decorators(object):
+    @classmethod
+    def cleaning_up(cls, func):
+        @wraps(func)
+        def wrapped(*args, **kwargs):
+            GPIO.cleanup()
+            func(*args, **kwargs)
+            GPIO.cleanup()
+            # return res
+        return wrapped
 
 
 class Pump:
-    def __init__(self):
-        pass
+    def __init__(self, channel=12, auto_turn_off=5):
+        self.channel = channel
+        self.turn_off_time = auto_turn_off
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.channel, GPIO.OUT)
 
+    @staticmethod
+    def clean():
+        GPIO.cleanup()
 
-channel = 21
+    @Decorators.cleaning_up
+    def turn_off(self):
+        GPIO.output(self.channel, GPIO.HIGH)
 
-# GPIO setup
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(channel, GPIO.OUT)
-
-
-def motor_on(pin):
-    GPIO.output(pin, GPIO.HIGH)  # Turn motor on
-
-
-def motor_off(pin):
-    GPIO.output(pin, GPIO.LOW)  # Turn motor off
+    @Decorators.cleaning_up
+    def turn_on(self):
+        GPIO.output(self.channel, GPIO.LOW)
+        time.sleep(self.turn_off_time)
 
 
 if __name__ == '__main__':
-    try:
-        motor_on(channel)
-        time.sleep(1)
-        motor_off(channel)
-        time.sleep(1)
-        GPIO.cleanup()
-    except KeyboardInterrupt:
-        GPIO.cleanup()
+    pump = Pump()
+    pump.turn_on()
+    
+    # try:
+    #     motor_on(channel)
+    #     time.sleep(1)
+    #     motor_off(channel)
+    #     time.sleep(1)
+    #     GPIO.cleanup()
+    # except KeyboardInterrupt:
+    #     GPIO.cleanup()
